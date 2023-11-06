ddf1bdb4da84a0b7e0de7dd35969cb5f07fe4098
TMason11095 <tmason11095@gmail.com>
2023-11-06 17:13:39 -0600

    Refactored etch-a-sketch to use jQuery

--- etch-a-sketch/javascript.js
+++ etch-a-sketch/javascript.js
@@ -1,12 +1,12 @@
-const SKETCH_CONTAINER = document.querySelector('#sketch-container');
-const SKETCH_CONTAINER_SIZE = getComputedStyle(document.body).getPropertyValue('--sketch-container-size');
-const GENERATE_BUTTON = document.querySelector('#generate-grid');
+const SKETCH_CONTAINER = $('#sketch-container');
+const SKETCH_CONTAINER_SIZE = $('body').css('--sketch-container-size');
+const GENERATE_BUTTON = $('#generate-grid');
 const SQUARE_SIZE_MIN = 1;
 const SQUARE_SIZE_MAX = 100;
-const SQUARE_SIZE_INPUT = document.querySelector('#square-size-input');
+const SQUARE_SIZE_INPUT = $('#square-size-input');
 
 (function setupGridListener() {
-	SKETCH_CONTAINER.onmouseover = function (e) {
+	SKETCH_CONTAINER.mouseover(function (e) {
 		//Ignore everything that isn't a block
 		if (e.target.getAttribute('name') != 'block') {
 			return;
@@ -14,22 +14,18 @@ const SQUARE_SIZE_INPUT = document.querySelector('#square-size-input');
 
 		//Add the black background class to the block
 		e.target.classList.add('black-background');
-	}
+	})
 })();
 
 (function setupGenerateButton() {
 	//Add click event
-	GENERATE_BUTTON.onclick = () => {
+	GENERATE_BUTTON.click(() => {
 		//Clear the grid
-		//Loop till out of children
-		while (SKETCH_CONTAINER.firstChild != null) {
-			//Remove the last child
-			SKETCH_CONTAINER.removeChild(SKETCH_CONTAINER.lastChild);
-		}
+		SKETCH_CONTAINER.empty();
 
 		//Fill in the new grid
 		//Get the number of blocks per side
-		const squaresPerSide = SQUARE_SIZE_INPUT.valueAsNumber;
+		const squaresPerSide = SQUARE_SIZE_INPUT.val();
 		//Check if valid number
 		if (isNaN(squaresPerSide)) {
 			alert("Please enter a valid number.");
@@ -42,37 +38,30 @@ const SQUARE_SIZE_INPUT = document.querySelector('#square-size-input');
 		}
 		//Set block size based on the given count
 		const blockSize = parseInt(SKETCH_CONTAINER_SIZE) / squaresPerSide;
-		document.documentElement.style.setProperty("--block-size", blockSize + "px");
+		$('html').css("--block-size", blockSize + "px");
 		//Set row and column count
 		const rowCount = squaresPerSide;
 		const columnCount = squaresPerSide;
 		//Build the grid
 		for (let i = 0; i < rowCount; i++) {
 			//Create row
-			const sketchRow = document.createElement('div');
-			//Set attributes
-			sketchRow.setAttribute('class', "flex center");
+			const sketchRow = $('<div class="flex center"></div>');
 
 			//Create column blocks
 			for (let j = 0; j < columnCount; j++) {
 				//Create block
-				const block = document.createElement('div');
-				//Set attributes
-				block.setAttribute('name', "block");
+				const block = $('<div name="block"></div>')
 				//Add block to row
-				sketchRow.appendChild(block);
+				sketchRow.append(block);
 			}
 
 			//Add the row to the sketch container
-			SKETCH_CONTAINER.appendChild(sketchRow);
+			SKETCH_CONTAINER.append(sketchRow);
 		}
-		console.log(blockSize);
-	}
+	})
 })();
 
 (function triggerGenerateEvent() {
-	//Create the onclick event
-	const onclickEvent = new Event("click");
-	//Trigger the event
-	GENERATE_BUTTON.dispatchEvent(onclickEvent);
+	//Trigger the button's click event
+	GENERATE_BUTTON.trigger('click');
 })();
